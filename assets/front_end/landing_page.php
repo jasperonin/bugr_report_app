e30758d8670eba67bbcee23834266410ba717510
Leo <snzleo@gmail.com>
2024-08-24 15:02:44 +0800

    Added landing Page

---
+++ assets/front_end/landing_page.php
@@ -0,0 +1,72 @@
+<!doctype html>
+
+<html lang="en">
+
+
+
+<meta charset="utf-8">
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>Landing page</title>
+
+<link href="./style.css" rel="stylesheet">
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+
+
+
+<style>
+    #nav_header {
+
+        color: #3077bd;
+
+        letter-spacing: 5px;
+
+        font-weight: 1000;
+
+        font-size: 150px;
+
+    }
+</style>
+
+
+
+</head>
+
+<body>
+
+    <main>
+        <div class="container d-sm-inline-flex">
+            <div class="row justify-content-start no-gutters">
+                <div class="col-md-7">
+                    <div class="card-body bg-transparent border-0 text-center text-wrap fs-5 fw-0" style="letter-spacing:1px; color:rgb(64,64,64);">
+                        <img src="https://i.ibb.co/1zM2ZVF/Untitled-design-7.png" class="img-fluid float-left">
+                    </div>
+                </div>
+
+                <div class="align-content-center col-lg-5">
+                    <div class="card-body bg-transparent border-0 text-left text-wrap fs-5 fw-2" style="letter-spacing:1px; color:rgb(64,64,64);">
+
+                        <!--<img src="https://i.ibb.co/xCWcjr6/Untitled-design-3.png" class="img-fluid">   -->
+
+                        <img src="https://cdn.prod.website-files.com/621d1905031cff21e84611fa/621fbfe243634aebf60d16b5_logo.svg">
+                        <h1 id="nav_header">BugHotel</h1>
+                        <hr class="my-0 border-0">
+                        "The hotel that you'd never want to visit!â€ - MJ Mandal
+                        <div class="text-left">
+                            <button href="/home_page" class="btn btn-primary  mt-3 rounded-pill">Get Started</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </main>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
